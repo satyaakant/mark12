34dfbcf237ca4905aa3d4d3a92a9f1082a52f3a2
satyaaa <satyakantmishra50@gmail.com>
2022-09-26 21:05:29 +0530

    debug

--- quiz.js
+++ quiz.js
@@ -2,7 +2,7 @@ const quizForm = document.querySelector(".Quiz-form");
 const submitAnswer = document.querySelector("#btn-submit");
 const outputle = document.querySelector("#output");
 
-const correctAnswer = ["75°","Right Angled","Equilateral Triangle","180°"];
+const correctAnswer = ["75°","Right Angled","Equilateral Triangle","180"];
 
 function calculateScore(){
     let score =0;
